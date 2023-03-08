6b1d7804e91cd3c861f1db7e87d0a2f1112ed1d3
Kaych3e <nkaduba@gmail.com>
2023-03-08 09:07:55 +0000

    Airbnb

---
+++ models/base_model.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""This File defines the BaseModel class that will serve as
+the base class for all our models"""
+
+import models
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+"""Base class for all our classes in this project"""
+
+    def __init__(self, *args, **kwargs):
+        """initialises a new Basemodel
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pair of attributes."""
+
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue;
+                elif key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, tform)
+                else:
+                    self.__dict__[key] = value
+        else:
+            models.storage.new(self)
+
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """Return the dictionary of the BaseModel instance.
+
+        This includes the key/value pair __class__ representing
+        the class name of the object."""
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+	dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
